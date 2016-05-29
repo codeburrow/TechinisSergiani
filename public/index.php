2d2b0dce7c32b5d3215f8eebc1cc9a9ad593e82f
Antony Kalogeropoulos <anthonykalogeropoulos@gmail.com>
2016-05-29 23:06:46 +0300

    Install and initiate Slim

--- public/index.php
+++ public/index.php
@@ -8,14 +8,16 @@
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../app/setup.php';
 
-use Pux\Mux;
-use Pux\Executor;
 use Kourtis\Controllers\HelloController;
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
 
-$mux = new Mux;
-$mux->get('/', ['Kourtis\Controllers\HelloController','index']);
-//$mux->post('/post', ['HelloController','helloAction']);
-//$mux->put('/put', ['HelloController','helloAction']);
+$app = new \Slim\App;
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
 
-$route = $mux->dispatch( $_SERVER['REQUEST_URI'] );
-echo Executor::execute($route);
+    return $response;
+});
+$app->run();
+
