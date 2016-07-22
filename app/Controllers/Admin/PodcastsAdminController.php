cb9ea365325193a758547dcf76c15a1078beaa4c
Antony Kalogeropoulos <anthonykalogeropoulos@gmail.com>
2016-07-22 22:06:13 +0300

    Feature: Create podcasts post | Admin Panel

--- app/Controllers/Admin/PodcastsAdminController.php
+++ app/Controllers/Admin/PodcastsAdminController.php
@@ -2,6 +2,7 @@
 namespace Kourtis\Controllers\Admin;
 
 use Kourtis\Database\PodcastsDB;
+use Kourtis\Services\UploadImageService;
 
 class PodcastsAdminController extends AdminController
 {
@@ -17,9 +18,36 @@ class PodcastsAdminController extends AdminController
             echo $this->twig->render('login.twig');
     }
 
-    public function postAddPost(){
-        //ToDo
+    public function postAddPost()
+    {
+        $DB = new PodcastsDB();
+        $uploadImageService = new uploadImageService();
+        $success = false;
+
+        //Try to upload image
+        $uploadError = $uploadImageService->uploadImage('img/podcasts/');
+        if (empty($uploadError)) {
+
+            //Add row to db
+            $nameOfImage = $_FILES['image']['name'];
+            $result = $DB->addPost($_POST, $nameOfImage);
+
+            if (empty($result)) { //successfully added row
+                $flashMessage = "Post Succesfully Added";
+                $success = true;
+            } else { //failed to add row
+                $flashMessage = $result;
+                //Delete uploaded image from server
+                unlink("img/podcasts/$nameOfImage");
+            }
+
+        } else { //image failed to upload
+            $flashMessage = $uploadError . "\nError: Could not upload image.";
+        }
+
+        echo $this->twig->render('podcasts/addPodcastPost.twig', array('flashMessage' => $flashMessage, 'success' => $success));
     }
+
 
     public function editPost(){
         if ($this->adminIsLoggedIn()){
